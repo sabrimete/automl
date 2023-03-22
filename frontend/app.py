4b3b7a895e32e47b410b9e56ae42f2c83858a7c4
basakappcircle <basak@appcircle.io>
2023-03-22 17:01:22 +0300

    streamlit with httpx for http2

--- frontend/app.py
+++ frontend/app.py
@@ -1,29 +1,24 @@
-import nicegui as ng
-import requests
+import streamlit as st
+import httpx
 import pandas as pd
 import io
 import json
-# Define receive and send functions for NiceGUI communication
-def receive():
-    return ng.get_events()
 
-def send(update):
-    ng.update(update)
-
-ng.app("CMPE492AutoML Project", receive=receive, send=send)
-
+st.title('CMPE492AutoML Project')
 
 # Set FastAPI endpoint
 # endpoint = 'http://localhost:8000/predict'
 predict_endpoint = 'https://backend-6r72er7ega-uc.a.run.app/predict' # Specify this path for Dockerization to work
 train_endpoint = 'https://backend-6r72er7ega-uc.a.run.app/train'
 
-# ng.html("<h2>Train Dataset</h2>")
-train_csv = ng.file_uploader(" ", extensions=["csv"])
-if train_csv is not None:
+
+st.subheader('Train Dataset')
+train_csv = st.file_uploader(' ', type=['csv'], accept_multiple_files=False)
+
+if train_csv:
     train_df = pd.read_csv(train_csv)
-    ng.subheader("Sample of Uploaded Dataset")
-    ng.table(train_df.head())
+    st.subheader('Sample of Uploaded Dataset')
+    st.write(train_df.head())
 
     # Convert dataframe to BytesIO object (for parsing as file into FastAPI later)
     train_bytes_obj = io.BytesIO()
@@ -33,24 +28,31 @@ if train_csv is not None:
     files = {"file": ('train_dataset.csv', train_bytes_obj, "multipart/form-data")}
 
     # Upon click of button
-    if ng.button('Start Train'):
+    if st.button('Start Train'):
         if len(train_df) == 0:
-            ng.markdown("## Train Dataset")
+            st.write("Please upload a valid Train dataset!")  # handle case with no image
         else:
-            with ng.spinner('Training in Progress. Please Wait...'):
-                output = requests.post(train_endpoint, 
-                                       files=files,
-                                       timeout=8000)
-            ng.success('Success!')
-            # ng.text(output)
-            # ng.text(output.json())
+            with st.spinner('Training in Progress. Please Wait...'):
+                with httpx.Client(http2=True) as client:
+                    response = client.post(train_endpoint,
+                                           files=files,
+                                           timeout=8000)
+            st.success('Success!')
+            st.write(response)
+            st.write(response.json())
+            # pd.DataFrame.from_dict(output.json(), orient="index")
+            # print(type(df))
+            # print(df)
+            # st.write(df)
 
-# ng.html("<h2>Test Dataset</h2>")
-test_csv = ng.file_uploader("  ", extensions=["csv"])
-if test_csv is not None:
+st.subheader('Test Dataset')
+test_csv = st.file_uploader('  ', type=['csv'], accept_multiple_files=False)
+
+# Upon upload of file (to test using test.csv from data/processed folder)
+if test_csv:
     test_df = pd.read_csv(test_csv)
-    ng.subheader("Sample of Uploaded Dataset")
-    ng.table(test_df.head())
+    st.subheader('Sample of Uploaded Dataset')
+    st.write(test_df.head())
 
     # Convert dataframe to BytesIO object (for parsing as file into FastAPI later)
     test_bytes_obj = io.BytesIO()
@@ -60,16 +62,19 @@ if test_csv is not None:
     files = {"file": ('test_dataset.csv', test_bytes_obj, "multipart/form-data")}
 
     # Upon click of button
-    if ng.button('Start Prediction'):
+    if st.button('Start Prediction'):
         if len(test_df) == 0:
-            ng.markdown("## Train Dataset")
+            st.write("Please upload a valid test dataset!")  # handle case with no image
         else:
-            with ng.spinner('Prediction in Progress. Please Wait...'):
-                output = requests.post(predict_endpoint, 
-                                       files=files,
-                                       timeout=8000)
-            ng.success('Success! Click Download button below to get prediction results (in JSON format)')
-            # ng.download(json.dumps(output.json()), "automl_prediction_results.json")
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8501))
-    ng.serve(port=port, address='0.0.0.0')
+            with st.spinner('Prediction in Progress. Please Wait...'):
+                with httpx.Client(http2=True) as client:
+                    response = client.post(predict_endpoint,
+                                           files=files,
+                                           timeout=8000)
+            st.success('Success! Click Download button below to get prediction results (in JSON format)')
+            # st.write(output.json()[1])
+            # st.download_button(
+            #     label='Download',
+            #     data=json.dumps(output.json()[0]['body']), # Download as JSON file object
+            #     file_name='automl_prediction_results.json'
+            # )
