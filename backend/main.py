b59ee65328e27c00d7dec74590cbd1ef69785c98
basakappcircle <basak@appcircle.io>
2023-03-31 16:54:23 +0300

    added new fields and saving option

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 import io
 import h2o
+import json
 from h2o.automl import H2OAutoML, get_leaderboard
 
-from fastapi import FastAPI, File, Form, UploadFile, Request
+from fastapi import FastAPI, File, Form, UploadFile, Request, Body
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,7 +14,8 @@ from google.cloud import storage
 from sklearn.metrics import accuracy_score
 
 app = FastAPI()
-
+global_leaderboard = None
+model = None
 # configure CORS middleware
 origins = ["*"]  # Replace * with your specific domain if you don't want to allow all domains
 app.add_middleware(
@@ -27,6 +29,24 @@ app.add_middleware(
 
 h2o.init()
 
+@app.post("/save_models")
+async def save_models(model_ids: list[str] = Body(...)):
+    global global_leaderboard
+
+    if global_leaderboard is None:
+        return {"message": "No leaderboard available"}
+
+    for model_id in model_ids:
+        model = h2o.get_model(model_id)
+        model_path = h2o.save_model(model=model, path='sample_model/', force=True)
+
+
+        # Perform any action you want with the selected models
+        # For example, you can save the models to a file or database
+    return {"message": model_path}
+    
+
+
 @app.post("/predict")
 async def predict(request: Request):
     form_data = await request.form()
@@ -35,7 +55,9 @@ async def predict(request: Request):
     predict_df = pd.read_csv(file_obj)
     predict_frame = h2o.H2OFrame(predict_df)
 
-    path = 'gs://deneme-bucket-1/model/XGBoost_1_AutoML_1_20230322_115208' 
+    # path = 'gs://deneme-bucket-1/model/XGBoost_1_AutoML_1_20230322_115208' 
+    global model
+    path = '/app/sample_model/' + model.model_id
     loaded_model = h2o.load_model(path)
 
     predictions = loaded_model.predict(predict_frame).as_data_frame()['predict'].values
@@ -47,7 +69,21 @@ async def predict(request: Request):
 async def train(request: Request):
     form_data = await request.form()
     file = form_data["file"].file
-    trainString = form_data["targetString"]
+    trainString = form_data.get("target_string")
+    max_runtime_secs = int(form_data.get("max_runtime_secs") or 300)
+    max_models = int(form_data.get("max_models") or 6)
+    # if(max_models == 1):
+    #     max_models = 2
+    nfolds = int(form_data.get("nfolds") or 5)
+    seed = int(form_data.get("seed") or 42)
+    include_algos = form_data.get("include_algos")
+    print(include_algos)
+    if include_algos:
+        include_algos = json.loads(include_algos)
+    else:
+        include_algos = None
+    print("PARAMS ",trainString, max_runtime_secs, max_models, nfolds, seed, include_algos)
+
     file_obj = io.BytesIO(file.read())
     train_df = pd.read_csv(file_obj)
     main_frame = h2o.H2OFrame(train_df)
@@ -55,15 +91,24 @@ async def train(request: Request):
     y = trainString
     x = [n for n in main_frame.col_names if n != y]
 
-    aml = H2OAutoML(max_models=1, seed=42, balance_classes=False)
+    aml = H2OAutoML(
+        max_runtime_secs=max_runtime_secs,
+        max_models=max_models,
+        nfolds=nfolds,
+        seed=seed,
+        include_algos=include_algos
+    )
     aml.train(x=x, y=y, training_frame=main_frame)
 
     lb = aml.leaderboard
+    global global_leaderboard
+    global_leaderboard = aml.leaderboard
     lb.head(rows=lb.nrows)
 
-    model = aml.leader
-    model_path = h2o.save_model(model=model, path='gs://deneme-bucket-1/model/', force=True)
-    print(model_path)
+    # model = aml.leader
+    # model_path = h2o.save_model(model=model, path='sample_model/', force=True)
+    # model_path = h2o.save_model(model=model, path='gs://deneme-bucket-1/model/', force=True)
+    # print(model_path)
 
     response =  aml.leaderboard.as_data_frame(use_pandas=True).to_json()
     print(response)
--- frontend/app.js
+++ frontend/app.js
@@ -1,21 +1,41 @@
-// const predict_endpoint = 'http://0.0.0.0:8000/predict';
-// const train_endpoint = 'http://0.0.0.0:8000/train';
+const predict_endpoint = 'http://0.0.0.0:8000/predict';
+const train_endpoint = 'http://0.0.0.0:8000/train';
+const save_endpoint = "http://0.0.0.0:8000/save_models"
 
-predict_endpoint = 'https://backend-6r72er7ega-uc.a.run.app/predict' 
-train_endpoint = 'https://backend-6r72er7ega-uc.a.run.app/train'
+// predict_endpoint = 'https://backend-6r72er7ega-uc.a.run.app/predict' 
+// train_endpoint = 'https://backend-6r72er7ega-uc.a.run.app/train'
 function uploadTrainFile() {
     const input = document.getElementById("trainFile");
     const file = input.files[0];
-    const targetString = document.getElementById("targetString").value;
+    const target_string = document.getElementById("target_string").value;
+    const max_runtime_secs = document.getElementById("max_runtime_secs").value;
+    const max_models = document.getElementById("max_models").value;
+    const nfolds = document.getElementById("nfolds").value;
+    const seed = document.getElementById("seed").value;
+    const algoSelect = document.getElementById("algo-select");
+    const selectedAlgos = Array.from(algoSelect.selectedOptions).map((option) => option.value);
 
-    if (targetString == '') {
+    console.log("selectedalgos ", selectedAlgos);
+
+    if (target_string == '') {
         alert('Please enter the name of the target column.');
         return;
     }
+    if (max_models!= 0 && selectedAlgos.length > max_models) {
+        alert('Max models value should be bigger than number of selected algorithms');
+        return;
+    }
+
 
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("targetString", targetString);
+    formData.append("target_string", target_string);
+    formData.append("max_runtime_secs", max_runtime_secs);
+    formData.append("max_models", max_models);
+    formData.append("nfolds", nfolds);
+    formData.append("seed", seed);
+    formData.append("include_algos", JSON.stringify(selectedAlgos));
+    console.log("selectedalgos ", JSON.stringify(selectedAlgos));
 
     showTrainStatus('Uploading train file...');
 
@@ -32,11 +52,9 @@ function uploadTrainFile() {
         }
     })
     .then(text => {
-        const jsonObject = JSON.parse(text);
-        const prettyJsonString = JSON.stringify(jsonObject, null, 4);
-        console.log(prettyJsonString);
-        document.getElementById('jsonLeaderboard').textContent = prettyJsonString;
+        displayLeaderboard(text, max_models);
     })
+
     .catch(error => {
         hideTrainStatus();
         console.error(error);
@@ -104,3 +122,196 @@ function hidePredictStatus() {
     const status = document.getElementById("predictStatus");
     status.textContent = '';
 }
+function displayLeaderboard(jsonText, model_num) {
+    var jsonObject = JSON.parse(jsonText);
+    var jsonObject = JSON.parse(jsonObject);
+    const leaderboard = document.createElement("table");
+    const header = leaderboard.createTHead();
+    const headerRow = header.insertRow();
+  
+    // Add the new column for the selection checkbox
+    const selectHeaderCell = document.createElement("th");
+    selectHeaderCell.textContent = "Select";
+    headerRow.appendChild(selectHeaderCell);
+  
+    const columns = [
+      "model_id",
+      "rmse",
+      "mse",
+      "mae",
+      "rmsle",
+      "mean_residual_deviance",
+    ];
+  
+    columns.forEach((column) => {
+      const headerCell = document.createElement("th");
+      headerCell.textContent = column;
+      headerRow.appendChild(headerCell);
+    });
+  
+    const tbody = document.createElement("tbody");
+    leaderboard.appendChild(tbody);
+  
+    const numRows = Object.keys(jsonObject["model_id"]).length;
+    for (let i = 0; i < numRows; i++) {
+        const row = tbody.insertRow();
+      
+        const checkboxCell = row.insertCell();
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.setAttribute("data-model-id", jsonObject["model_id"][i.toString()]);
+        checkboxCell.appendChild(checkbox);
+      
+        columns.forEach((column) => {
+          const cell = row.insertCell();
+          const key = column.replace(" ", "_").toLowerCase();
+          if (jsonObject[key] && jsonObject[key][i.toString()]) {
+            if (key === "model_id") {
+              cell.textContent = jsonObject[key][i.toString()];
+            } else {
+              cell.textContent = parseFloat(jsonObject[key][i.toString()]).toFixed(4);
+            }
+          } else {
+            cell.textContent = "";
+          }
+        });
+      }
+      
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Submit Selected Models";
+    // saveButton.addEventListener("click", () => saveSelectedModels(leaderboard));
+    saveButton.addEventListener("click", () => {
+        // Display a confirmation dialog box
+        const confirmed = confirm("Are you sure about the selected models?\nAll the models are gonna disappear after this step!");
+        
+        // Check if the user confirmed the action
+        if (confirmed) {
+            // Call the saveSelectedModels function
+            saveSelectedModels(leaderboard, saveButton);
+        } else {
+            // Do nothing
+        }
+    });
+    const container = document.getElementById("jsonLeaderboard");
+    container.innerHTML = "";
+    container.appendChild(leaderboard);
+    container.appendChild(saveButton); // Append the save button after the table
+  }
+
+  
+  function saveSelectedModels(table, saveButton) {
+    const checkboxes = table.querySelectorAll("input[type='checkbox']");
+    const selectedModels = [];
+  
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        const modelId = checkbox.getAttribute("data-model-id");
+        selectedModels.push(modelId);
+      }
+    });
+  
+    if (selectedModels.length > 0) {
+      console.log("Selected models:", selectedModels);
+      sendSelectedModels(selectedModels, table, saveButton);
+      // Send the selected model IDs to your server or perform any other actions
+    } else {
+      console.log("No models selected");
+    }
+  }
+  
+  async function sendSelectedModels(modelIds, table, saveButton) {
+    console.log(JSON.stringify(modelIds))
+    const response = await fetch(save_endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(modelIds),
+    })
+    .then(response => {
+        hidePredictStatus();
+        if (response.ok) {
+            console.log("Selected models sent successfully");
+            alert("Selected models are saved successfully");
+            table.innerHTML = "";
+            saveButton.remove();
+            
+            return response.text();
+        } else {
+            console.error("Error sending selected models");
+        }
+    })
+
+    .catch(error => {
+        hidePredictStatus();
+        console.error(error);
+    });
+  }
+  
+  
+  
+  
+// function displayLeaderboard(jsonText, model_num) {
+//     var jsonObject = JSON.parse(jsonText);
+//     var jsonObject = JSON.parse(jsonObject);
+//     const leaderboard = document.createElement("table");
+//     const header = leaderboard.createTHead();
+//     const headerRow = header.insertRow();
+  
+//     const columns = [
+//       "model_id",
+//       "rmse",
+//       "mse",
+//       "mae",
+//       "rmsle",
+//       "mean_residual_deviance",
+//     ];
+  
+//     columns.forEach((column) => {
+//       const headerCell = document.createElement("th");
+//       headerCell.textContent = column;
+//       headerRow.appendChild(headerCell);
+//     });
+  
+//     const tbody = document.createElement("tbody");
+//     leaderboard.appendChild(tbody);
+  
+//     const numRows = Object.keys(jsonObject["model_id"]).length;
+  
+//     for (let i = 0; i < numRows; i++) {
+//       const row = tbody.insertRow();
+//       columns.forEach((column) => {
+//         const cell = row.insertCell();
+//         const key = column.replace(" ", "_").toLowerCase();
+//         if (jsonObject[key] && jsonObject[key][i.toString()]) {
+//           if (key === "model_id") {
+//             cell.textContent = jsonObject[key][i.toString()];
+//           } else {
+//             cell.textContent = parseFloat(jsonObject[key][i.toString()]).toFixed(4);
+//           }
+//         } else {
+//           cell.textContent = "";
+//         }
+//       });
+//     }
+  
+//     const container = document.getElementById("jsonLeaderboard");
+//     container.innerHTML = "";
+//     container.appendChild(leaderboard);
+//   }
+
+//   function saveSelectedModels() {
+//     const table = document.getElementById("leaderboard");
+//     const checkboxes = table.querySelectorAll("input[type='checkbox']");
+//     const selectedModelIds = [];
+  
+//     checkboxes.forEach((checkbox) => {
+//       if (checkbox.checked) {
+//         selectedModelIds.push(checkbox.value);
+//       }
+//     });
+  
+//     // Send the selectedModelIds to your server or use it as needed
+//     console.log("Selected Model IDs: ", selectedModelIds);
+//   }
+  
