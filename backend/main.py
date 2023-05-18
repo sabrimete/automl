b8311597faac9028e269e8867de57272e0c4686f
Oğuzhan Demirel <49492680+oguzhandemirelx@users.noreply.github.com>
2023-05-18 23:53:42 +0300

    unsupervised train and manual mode supervised train added

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,11 @@ import pandas as pd
 import io
 import h2o
 import json
+import sklearn
 from h2o.automl import H2OAutoML, get_leaderboard
-
+from h2o.estimators import H2OKMeansEstimator
+from matplotlib import pyplot as plt
+from base64 import encodebytes
 
 from fastapi import FastAPI, File, Form, UploadFile, Request, Body
 from fastapi.encoders import jsonable_encoder
@@ -165,139 +168,319 @@ async def train(request: Request):
     return response
 
 @app.post("/unsupervised-train")
-async def train(request: Request):
+async def unsupervisedTrain(request: Request):
     form_data = await request.form()
-    file = form_data["file"].file
-    algo = form_data.get("algo")
-    predictors = [col for col in file.col_names]
-    if algo == "kmeans":
-        kmeans = h2o.estimators.H2OKMeansEstimator(
-            k=int(form_data.get("number_of_clusters") or 5),          # number of clusters
-            standardize=True,  # standardize the data before clustering
-            seed = int(form_data.get("seed") or 42)      # set the seed for reproducibility
-        )
-        kmeans.train(x=predictors, training_frame=file)
-    elif algo == "iforest":
-        iforest = h2o.estimators.H2OIsolationForestEstimator(
-            ntrees=int(form_data.get("ntrees") or 100),          # number of trees in the forest
-            max_depth=int(form_data.get("max_depth") or 20),        # maximum depth of each tree
-            sample_size=int(form_data.get("sample_size") or 256),     # size of the sample used to train each tree
-            seed=int(form_data.get("seed") or 42)           # set the seed for reproducibility
-        )
-        iforest.train(x=predictors, training_frame=file)
+    data = form_data["file"].file
+    file_obj = io.BytesIO(data.read())
+    data_pd = h2o.H2OFrame(pd.read_csv(file_obj))
+    # data_pd = h2o.H2OFrame(train_df)
+    # data_pd = h2o.H2OFrame(train_df.values.tolist(), column_names=train_df.columns.tolist())
 
-    experiment_name = 'deneme' + str(random.randint(1, 1000000))
-    try:
-        experiment_id = mlflow.create_experiment(experiment_name)
-        experiment = client.get_experiment_by_name(experiment_name)
-    except:
-        experiment = client.get_experiment_by_name(experiment_name)
+    # Convert the H2OFrame object to a pandas DataFrame
+    data_df = data_pd.as_data_frame()
 
-    mlflow.set_experiment(experiment_name)
+    # Extract the labels from the first line
+    labels = data_df.columns
 
-    # Print experiment details
-    print(f"Name: {experiment_name}")
-    print(f"Experiment_id: {experiment.experiment_id}")
-    print(f"Artifact Location: {experiment.artifact_location}")
-    print(f"Lifecycle_stage: {experiment.lifecycle_stage}")
-    print(f"Tracking uri: {mlflow.get_tracking_uri()}")
+    # Define the range of k values to try
+    k_values = range(2, 10)
+
+    # Initialize lists to store the evaluation scores
+    silhouette_scores = []
+    sse = []
+
+    # Perform k-means clustering for each k value
+    for k in k_values:
+        # Create the k-means model
+        kmeans = H2OKMeansEstimator(k=k, seed=123)
+        kmeans.train(training_frame=data_pd)
+
+        # Compute the silhouette score
+        predictions = kmeans.predict(data_pd)
+        silhouette_scores.append(sklearn.metrics.silhouette_score(data_df, predictions.as_data_frame(), metric='euclidean'))
 
 
-    with mlflow.start_run():
-        if algo == "kmeans":
-            model = h2o.get_model(kmeans.model_id)
-        elif algo == "iforest":
-            model = h2o.get_model(iforest.model_id)
-        # Log and save best model (mlflow.h2o provides API for logging & loading H2O models)
-        mlflow.h2o.log_model(model, artifact_path='model')
-        model_uri = mlflow.get_artifact_uri('model')
-        print(f'model saved in {model_uri}')
-    
-    return {"message": model_uri}
+        # Compute the sum of squared errors (SSE)
+        sse.append(kmeans.tot_withinss())
+
+    # Plot the elbow curve
+    plt.figure(figsize=(10, 6))
+    plt.plot(k_values, sse, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Sum of Squared Errors (SSE)')
+    plt.title('Elbow Method')
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    elbowImage = encodebytes(buffer.getvalue()).decode('ascii')
+    elbowImage = elbowImage.replace('\n', '')
+    buffer.close()
+
+    # Plot the silhouette scores
+    plt.figure(figsize=(10, 6))
+    plt.plot(k_values, silhouette_scores, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Silhouette Score')
+    plt.title('Silhouette Scores')
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    silhouetteImage = encodebytes(buffer.getvalue()).decode('ascii')
+    silhouetteImage = silhouetteImage.replace('\n', '')
+    buffer.close()
+
+    # Find the optimal value of k based on the silhouette score
+    optimal_k = k_values[np.argmax(silhouette_scores)]
+    print("Optimal value of k: ", optimal_k)
+
+    # Train the final k-means model with the optimal k value
+    final_kmeans = H2OKMeansEstimator(k=optimal_k, seed=123)
+    final_kmeans.train(training_frame=data_pd)
+
+    # Get the cluster assignments for each data point
+    predictions = final_kmeans.predict(data_pd)
+    clusters = predictions.as_data_frame()['predict'].values
+    centroids = np.array(final_kmeans.centers())
+
+    data_np = data_pd.as_data_frame().values
+    # Compute the distances of each data point to the closest centroid
+    distances = np.linalg.norm(data_np - centroids[clusters], axis=1)
+
+    # Set a threshold for outlier detection (adjust as needed)
+    threshold = np.percentile(distances, 95)  # 95th percentile as a threshold
+
+    # Identify outliers as data points with distances above the threshold
+    outliers = np.where(distances > threshold)[0]
+
+    responseCluster = []
+    responseOutliers = []
+    for i in range(optimal_k):
+        cluster_data = data_np[clusters == i]
+        cluster_outliers = cluster_data[np.isin(np.where(clusters == i)[0], outliers)]
+        responseCluster.append(cluster_data)
+        responseOutliers.append(cluster_outliers)
+
+    # Convert numpy arrays to nested lists
+    data = [arr.tolist() for arr in responseCluster]
+    responseCluster = {f"cluster {i + 1}": arr for i, arr in enumerate(data)}
+
+    data = [arr.tolist() for arr in responseOutliers]
+    responseOutliers = {f"outlier {i + 1}": arr for i, arr in enumerate(data)}
+
+    # # Print the cluster groups
+    # for i in range(optimal_k):
+    #     cluster_data = data_np[clusters == i]
+    #     cluster_outliers = cluster_data[np.isin(np.where(clusters == i)[0], outliers)]
+    #     print("Cluster ", i + 1, ":", cluster_data)
+    #     print("Outliers in Cluster ", i + 1, ":", cluster_outliers)
+
+    # Prepare the plot based on the number of dimensions
+    num_dimensions = data_pd.shape[1]
+
+    # Generate colors for clusters and outliers
+    num_clusters = len(np.unique(clusters))
+    cluster_colors = plt.cm.tab10(np.linspace(0, 1, num_clusters))
+    outlier_color = 'red'
+
+    finalImage = []
+    # Plot the data with clusters and outliers
+    if num_dimensions == 2:
+        # Plot for 2D data
+        plt.figure(figsize=(10, 6))
+        for i in range(optimal_k):
+            cluster_data = data_np[clusters == i]
+            plt.scatter(cluster_data[:, 0], cluster_data[:, 1], color=cluster_colors[i], alpha=0.7,
+                        label=f'Cluster {i + 1}')
+        plt.scatter(data_np[outliers, 0], data_np[outliers, 1], color=outlier_color, marker='x', label='Outliers')
+        plt.scatter(centroids[:, 0], centroids[:, 1], color='red', marker='*', s=200, edgecolors='k', label='Centroids')
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1])
+        plt.title('K-Means Clustering Result with Outliers')
+        plt.legend()
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        finalImage = encodebytes(buffer.getvalue()).decode('ascii')
+        finalImage = finalImage.replace('\n', '')
+        buffer.close()
+
+    elif num_dimensions == 3:
+        # Plot for 3D data
+        fig = plt.figure(figsize=(10, 6))
+        ax = fig.add_subplot(111, projection='3d')
+        for i in range(optimal_k):
+            cluster_data = data_np[clusters == i]
+            ax.scatter(cluster_data[:, 0], cluster_data[:, 1], cluster_data[:, 2], color=cluster_colors[i], alpha=0.7,
+                       label=f'Cluster {i + 1}')
+        ax.scatter(data_np[outliers, 0], data_np[outliers, 1], data_np[outliers, 2],
+                   color=outlier_color, marker='x', label='Outliers')
+        ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2],
+                   color='red', marker='*', s=200, edgecolors='k', label='Centroids')
+        ax.set_xlabel(labels[0])
+        ax.set_ylabel(labels[1])
+        ax.set_zlabel(labels[2])
+        ax.set_title('K-Means Clustering Result with Outliers')
+        ax.legend()
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        finalImage = encodebytes(buffer.getvalue()).decode('ascii')
+        finalImage = finalImage.replace('\n', '')
+        buffer.close()
+    else:
+        print("Cannot plot for data with more than 3 dimensions.")
+
+    response = {
+        "elbowImage": elbowImage,
+        "silhouetteImage": silhouetteImage,
+        "finalImage": finalImage,
+        "optimal_k": optimal_k,
+        "clusters": responseCluster,
+        "outliers": responseOutliers,
+    }
+
+    return response
 
 
-@app.post("/dev-supervised-train")
-async def train(request: Request):
+@app.post("/manual-supervised-train")
+async def manualSupervisedTrain(request: Request):
     form_data = await request.form()
-    file = form_data["file"].file
+    a = form_data["file"].file
+    file_obj = io.BytesIO(a.read())
+    file = h2o.H2OFrame(pd.read_csv(file_obj))
     algo = form_data.get("algo")
-    response = form_data.get("response")
+    response = form_data.get("target_string")
     predictors = [col for col in file.col_names if col != response]
 
     # Split the data into training and validation sets
     train, valid = file.split_frame(ratios=[0.7], seed=1234)
 
+    gbmGridSearchParameters = {
+        "ntrees": [10, 20, 30],
+        "max_depth": [10, 20, 30],
+        "learn_rate": [0.1, 0.2, 0.3],
+    }
+
+    glmGridSearchParameters = {
+        "alpha": [0.1, 0.2, 0.3],
+        "lambda": [0.1, 0.2, 0.3],
+    }
+
+    xgbGridSearchParameters = {
+        "ntrees": [10, 20, 30],
+        "max_depth": [10, 20, 30],
+        "learn_rate": [0.1, 0.2, 0.3],
+    }
+
+    rfGridSearchParameters = {
+        "ntrees": [10, 20, 30],
+        "max_depth": [10, 20, 30]
+    }
+
     if algo == "gbm":
         gbm = h2o.estimators.H2OGradientBoostingEstimator(
-            ntrees=int(form_data.get("ntrees") or 100),          # number of trees in the forest
-            max_depth=int(form_data.get("max_depth") or 20),         # maximum depth of each tree
-            learn_rate=float(form_data.get("learn_rate") or 0.1),      # learning rate of the algorithm
-            seed=int(form_data.get("seed") or 42)                # set the seed for reproducibility
+            min_rows=(form_data.get("min_rows") or 1),
+            sample_rate=(form_data.get("sample_rate") or 1),
+            col_sample_rate=(form_data.get("col_sample_rate") or 1),
+            min_split_improvement=(form_data.get("min_split_improvement") or 0.01),
+            distribution=str(form_data.get("distribution") or "gaussian"),
+            seed=int(42),
         )
-        gbm.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+
+        gridSearch = h2o.grid.H2OGridSearch(gbm, gbmGridSearchParameters)
+        gridSearch.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+        best_model = gridSearch.get_grid(sort_by='rmse', decreasing=False).models[0]  # get best model
+        global global_leaderboard
+
+        response = {
+            "model_id": {"0": best_model.model_performance(valid)._metric_json["model"]["name"]},
+            "rmse": {"0": best_model.rmse()},
+            "mse": {"0":best_model.mse()},
+            "mae": {"0":best_model.mae()},
+            "rmsle": {"0":best_model.rmsle()},
+            "mean_residual_deviance": {"0": best_model.mean_residual_deviance()},
+        }
+        global_leaderboard = response
+        return response
     elif algo == "glm":
         # Create a GLM model
         glm = h2o.estimators.H2OGeneralizedLinearEstimator(
-            family=str(form_data.get("family") or "binomial"),   # use binomial family for binary classification
-            alpha=float(form_data.get("alpha") or 0.5),           # specify the alpha regularization parameter
-            lambda_=float(form_data.get("lambda") or 1e-5),        # specify the lambda regularization parameter
-            seed=int(form_data.get("seed") or 42)            # set the seed for reproducibility
+            family=str(form_data.get("family") or "binomial"),  # use binomial family for binary classification
+            max_iterations=(form_data.get("max_iterations") or 100),
+            link=str(form_data.get("link") or "logit"),
+            missing_values_handling=str(form_data.get("missing_values_handling") or "MeanImputation"),
+            seed=int(42)  # set the seed for reproducibility
         )
-        # Train the GLM model on the training set
-        glm.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+
+        gridSearch = h2o.grid.H2OGridSearch(glm, glmGridSearchParameters)
+        gridSearch.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+        best_model = gridSearch.get_grid(sort_by='rmse', decreasing=False).models[0]  # get best model
+
+        response = {
+            "model_id": {"0": best_model.model_performance(valid)._metric_json["model"]["name"]},
+            "rmse": {"0": best_model.model_performance(valid)._metric_json["RMSE"]},
+            "mse": {"0":best_model.model_performance(valid)._metric_json["MSE"]},
+            "mae": {"0": None},
+            "rmsle": {"0": None},
+            "mean_residual_deviance": {"0": None},
+        }
+        global_leaderboard = response
+        return response
+
     elif algo == "xgb":
         # Create an XGBoost model
         xgb = h2o.estimators.H2OXGBoostEstimator(
-            ntrees=int(form_data.get("ntrees") or 100),          # number of trees in the forest
-            max_depth=int(form_data.get("max_depth") or 5),         # maximum depth of each tree
-            learn_rate=float(form_data.get("learn_rate") or 0.1),      # learning rate of the algorithm
-            seed=int(form_data.get("seed") or 42)         # set the seed for reproducibility
+            seed=int(42),  # set the seed for reproducibility
+            min_rows=(form_data.get("min_rows") or 1),
+            sample_rate=(form_data.get("sample_rate") or 1),
+            col_sample_rate=(form_data.get("col_sample_rate") or 1),
+            min_split_improvement=(form_data.get("min_split_improvement") or 0.01),
+            booster=str(form_data.get("booster") or "gbtree"),
         )
-        # Train the XGBoost model on the training set
-        xgb.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+
+        gridSearch = h2o.grid.H2OGridSearch(xgb, xgbGridSearchParameters)
+        gridSearch.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+        best_model = gridSearch.get_grid(sort_by='rmse', decreasing=False).models[0]  # get best model
+
+        response = {
+            "model_id": {"0": best_model.model_performance(valid)._metric_json["model"]["name"]},
+            "rmse": {"0": best_model.rmse()},
+            "mse": {"0": best_model.mse()},
+            "mae": {"0": best_model.mae()},
+            "rmsle": {"0": best_model.rmsle()},
+            "mean_residual_deviance": {"0": best_model.mean_residual_deviance()},
+        }
+        global_leaderboard = response
+        return response
+
     elif algo == "rf":
         # Create a Random Forest model
         rf = h2o.estimators.H2ORandomForestEstimator(
-            ntrees=int(form_data.get("ntrees") or 100),          # number of trees in the forest
-            max_depth=int(form_data.get("max_depth") or 5),         # maximum depth of each tree
-            min_rows=int(form_data.get("min_rows") or 10),         # maximum depth of each tree      # specify the minimum number of rows in each leaf node
-            seed=int(form_data.get("seed") or 42)         # set the seed for reproducibility
+            min_rows=int(form_data.get("min_rows") or 10),
+            sample_rate=(form_data.get("sample_rate") or 1),
+            col_sample_rate_per_tree=(form_data.get("col_sample_rate_per_tree") or 1),
+            min_split_improvement=(form_data.get("min_split_improvement") or 0.01),
+            mtries=(form_data.get("mtries") or -1),
+            # maximum depth of each tree      # specify the minimum number of rows in each leaf node
+            seed=int(42)  # set the seed for reproducibility
         )
-        # Train the Random Forest model on the training set
-        rf.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
 
-    experiment_name = 'deneme' + str(random.randint(1, 1000000))
-    try:
-        experiment_id = mlflow.create_experiment(experiment_name)
-        experiment = client.get_experiment_by_name(experiment_name)
-    except:
-        experiment = client.get_experiment_by_name(experiment_name)
+        gridSearch = h2o.grid.H2OGridSearch(rf, rfGridSearchParameters)
+        gridSearch.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+        best_model = gridSearch.get_grid(sort_by='rmse', decreasing=False).models[0]  # get best model
 
-    mlflow.set_experiment(experiment_name)
-
-    # Print experiment details
-    print(f"Name: {experiment_name}")
-    print(f"Experiment_id: {experiment.experiment_id}")
-    print(f"Artifact Location: {experiment.artifact_location}")
-    print(f"Lifecycle_stage: {experiment.lifecycle_stage}")
-    print(f"Tracking uri: {mlflow.get_tracking_uri()}")
-
-
-    with mlflow.start_run():
-        if algo == "gbm":
-            model = h2o.get_model(gbm.model_id)
-        elif algo == "glm":
-            model = h2o.get_model(glm.model_id)
-        elif algo == "xgb":
-            model = xgb.get_model(xgb.model_id)
-        elif algo == "rf":
-            model = h2o.get_model(rf.model_id)
-        # Log and save best model (mlflow.h2o provides API for logging & loading H2O models)
-        mlflow.h2o.log_model(model, artifact_path='model')
-        model_uri = mlflow.get_artifact_uri('model')
-        print(f'model saved in {model_uri}')
-    
-    return {"message": model_uri}
+        response = {
+            "model_id": {"0": best_model.model_performance(valid)._metric_json["model"]["name"]},
+            "rmse": {"0": best_model.model_performance(valid)._metric_json["RMSE"]},
+            "mse": {"0": best_model.model_performance(valid)._metric_json["MSE"]},
+            "mae": {"0": best_model.model_performance(valid)._metric_json["mae"]},
+            "rmsle": {"0": best_model.model_performance(valid)._metric_json["mae"]},
+            "mean_residual_deviance": {"0": best_model.model_performance(valid)._metric_json["mean_residual_deviance"]},
+        }
+        global_leaderboard = response
+        return response
 
 @app.get("/")
 async def main():
